42ccf72653812861736b74985889da0d2207184c
Raiyan <raiyanriz11@gmail.com>
2025-05-02 12:06:24 +0400

    inital commit

---
+++ Longformer/code/test.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("CUDA_VISIBLE_DEVICES=0 python run_longformer.py \
+        --output_dir=../saved_models/MutantEq \
+        --config_name=allenai/longformer-base-4096\
+        --model_name_or_path=allenai/longformer-base-4096 \
+        --tokenizer_name=allenai/longformer-base-4096\
+        --requires_grad 0 \
+        --do_test \
+        --code_db_file=../../dataset/EFSM_single/code_db.csv \
+        --train_data_file=../../dataset/EFSM_single/pairwise_train.csv \
+        --eval_data_file=../../dataset/EFSM_single/pairwise_test.csv \
+        --test_data_file=../../dataset/EFSM_single/pairwise_test.csv 2>&1")
---
+++ UniXCoder/code/train.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("CUDA_VISIBLE_DEVICES=0 python run.py \
+        --output_dir=saved_models/MutantEq \
+        --config_name=microsoft/unixcoder-base\
+        --model_name_or_path=microsoft/unixcoder-base \
+        --tokenizer_name=microsoft/unixcoder-base\
+        --requires_grad 0 \
+        --do_train \
+        --code_db_file=../../dataset/EFSM_single/code_db.csv \
+        --train_data_file=../../dataset/EFSM_single/pairwise_train.csv \
+        --eval_data_file=../../dataset/EFSM_single/pairwise_test.csv \
+        --test_data_file=../../dataset/EFSM_single/pairwise_test.csv 2>&1")
